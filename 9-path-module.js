d991d97fc9759296b7edcc5c31ba8552cda60818
ellijs <ellijs@hotmail.com>
2022-01-10 18:44:47 -0500

    path

--- 9-path-module.js
+++ 9-path-module.js
@@ -2,11 +2,16 @@ const path = require('path')
 
 console.log(path.sep)
 
-const filePath = path.join('/content/', 'subfolder', 'test.txt')
+// join path
+const filePath = path.join('/content', 'subfolder', 'test.txt')
 console.log(filePath)
 
+// basename
 const base = path.basename(filePath)
 console.log(base)
 
+//absolute path
 const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
 console.log(absolute)
+
+
