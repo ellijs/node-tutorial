72ab08b19fc1b16caa869895afce2ff7bc186c24
ellijs <ellijs@hotmail.com>
2022-01-10 20:15:41 -0500

    npm

--- npm.js
+++ npm.js
@@ -4,9 +4,16 @@
 // local dependency - use it only in this particular project
 // npm i <packageName>
 
+// nodemon app.js
+// zsh: command not found: nodemon
+
 // global dependency - use it in any project
 // npm install -g <packageName>
 // sudo npm install -g <packageName> (mac)
+
+// sudo npm install -g nodemon
+// nodemon app.js
+// NOW it's working
 
 // package.json - manifest file (stores important info about project/package)
 // manual approach ( create package.json in the root, create propertires etc)
@@ -20,3 +27,11 @@ const newItems = _.flattenDeep(items)
 
 console.log(newItems)
 // => [ 1, 2, 3, 4 ]
+
+
+// using gitignore, how can user use modules?
+// run npm install => npm check dependencies and automatically set up modules
+
+// npm i nodemon -D  
+// create short cut in "Script"
+// for deployment, nodemon refresh the server everytime it changes
